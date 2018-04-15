7a55dabd0db0a02da3ada4e31466de4f06cd5ded
Noam <noam@fireflymigration.com>
2018-04-15 19:44:54 +0300

    fixed production server reference

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ const serverUrl= '/';
 export const environment = {
   production: true,
   serverUrl,
-  dataSource: new radweb.LocalStorageDataProvider() as radweb.DataProviderFactory
-  //dataSource : new radweb.RestDataProvider(serverUrl+ 'dataapi') as radweb.DataProviderFactory
+  //dataSource: new radweb.LocalStorageDataProvider() as radweb.DataProviderFactory
+  dataSource : new radweb.RestDataProvider(serverUrl+ 'dataApi') as radweb.DataProviderFactory
 };
