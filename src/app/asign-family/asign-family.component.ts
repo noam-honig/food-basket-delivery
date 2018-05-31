3ffdab004fd7b8246aefe3ae2f6b75d1bb612d5c
Noam <noam@fireflymigration.com>
2018-05-31 17:28:01 +0300

    started more work on assign family

--- src/app/asign-family/asign-family.component.ts
+++ src/app/asign-family/asign-family.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GridSettings } from 'radweb';
-import { Families } from '../models';
+import { Families, Language } from '../models';
 import { AuthService } from '../auth/auth-service';
+import { SelectService } from '../select-popup/select-service';
 
 @Component({
   selector: 'app-asign-family',
@@ -10,14 +11,27 @@ import { AuthService } from '../auth/auth-service';
 })
 export class AsignFamilyComponent implements OnInit {
 
-
+  filterLangulage = -1;
+  langulages: Language[]= [
+    Language.Hebrew,
+    Language.Amharit,
+    Language.Russian
+  ];
+  phone: string;
+  name: string;
+  findHelper() {
+    this.dialog.selectHelper(h => {
+      this.phone = h.phone.value;
+      this.name = h.name.value;
+    });
+  }
   families = new GridSettings(new Families(), {
     get: {
       where: f => f.courier.isEqualTo(this.auth.auth.info.helperId),
-      limit:9999
+      limit: 9999
     }
   });
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private dialog: SelectService) { }
 
   ngOnInit() {
     this.families.getRecords();
