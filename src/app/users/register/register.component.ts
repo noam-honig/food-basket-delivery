01c069c0f4abb3beed6a92f6e2acb2a488d431ae
Noam <noam@fireflymigration.com>
2018-07-22 10:54:47 +0300

    fixed build

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -45,7 +45,7 @@ export class RegisterComponent implements OnInit {
     try {
       let userInfo = this.helpers.currentRow;
       await this.helpers._doSavingRow(userInfo);
-      this.auth.login(userInfo.phone.value, this.confirmPassword.value, false);
+      this.auth.login(userInfo.phone.value, this.confirmPassword.value, false,()=>{});
     }
     catch (err) {
       console.log(err);
