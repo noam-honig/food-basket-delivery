36439a4c266d518497f1c270f37760d9ecc30906
Noam <noam@fireflymigration.com>
2018-07-25 17:49:11 +0300

    יבוא מאקסל

--- src/app/server/doSomething.ts
+++ src/app/server/doSomething.ts
@@ -1,21 +1,23 @@
 import { Families } from "../models";
 import { readFileSync, readFile } from "fs";
-import {  ColumnHashSet } from "radweb";
+import { ColumnHashSet } from "radweb";
 
 import { GetGeoInformation } from "../shared/googleApiHelpers";
-import { foreachEntityItem } from "../shared/utils";
+import { foreachEntityItem, foreachSync } from "../shared/utils";
 
 import { serverInit } from "./serverInit";
+import * as XLSX from 'xlsx';
 
 serverInit();
 
 export async function DoIt() {
     try {
-     
-        
+
+
+
 
         console.log('123');
-//        new SendSmsUtils().sendSms('0507330590', 'test2');
+        //        new SendSmsUtils().sendSms('0507330590', 'test2');
     }
     catch (err) {
         console.log(err);
@@ -34,6 +36,44 @@ async function getGeolocationInfo() {
         }
 
     });
+}
+async function ImportFromExcel(){
+    
+    let wb = XLSX.readFile("C:\\Users\\Yoni\\Downloads\\xxx.xlsx");
+    let s = wb.Sheets[wb.SheetNames[1]];
+    let o = XLSX.utils.sheet_to_json(s);
+    let found = true;
+    await foreachSync(o, async r => {
+        try {
+
+            let f = new Families();
+            let get = x => {
+                if (!r[x])
+                    return '';
+                return r[x];
+            };
+            f.appartment.value = r["דירה"];
+            f.address.value = (get("כתובת") + ' ' + get("מספר").trim() + ' ' + get("עיר"));
+            f.familyMembers.value = +r["מס' נפשות"];
+            f.name.value = (get("שם משפחה") + " " + get("שם פרטי")).trim();
+            if (!f.name.value) {
+                f.name.value = '!ללא שם ';
+            }
+            f.phone1.value = r["טלפון"];
+            f.addressComment.value = r["הערות"];
+            if (found) {
+                await f.doSaveStuff({});
+                await f.save();
+            }
+            else if (f.address.value == 'השילוח 16 חולון')
+                found = true;
+        }
+        catch (err) {
+            console.log(err, o);
+        }
+
+    });
+
 }
 
 async function updateAddress() {
@@ -64,15 +104,15 @@ function UpdateAllFamiliyNames() {
 
     });
 }
-async function imprortFamiliesFromJson(){
+async function imprortFamiliesFromJson() {
     let r = readFileSync(`c:\\temp\\hugmoms.json`);
     var rows = JSON.parse(r.toString());
-    for (let i=0;i<rows.length;i++){
+    for (let i = 0; i < rows.length; i++) {
         let f = new Families();
         let c = new ColumnHashSet();
-        f.__fromPojo(rows[i],c);
-        let families =await  f.source.find({where:f.id.isEqualTo(f.id.value)});
-        if (families.length==0){
+        f.__fromPojo(rows[i], c);
+        let families = await f.source.find({ where: f.id.isEqualTo(f.id.value) });
+        if (families.length == 0) {
             await f.save();
         }
     }
