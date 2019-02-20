73d16915c55ef1c3b64f2a862bc395d38ce16d1d
Noam <noam@fireflymigration.com>
2019-02-20 13:50:13 +0200

    weekly packer by familty

--- src/app/weekly-packer-by-family/weekly-packer-by-family.component.ts
+++ src/app/weekly-packer-by-family/weekly-packer-by-family.component.ts
@@ -18,8 +18,9 @@ export class WeeklyPackerByFamilyComponent implements OnInit {
 
   async ngOnInit() {
     this.deliveries = await this.context.for(WeeklyFamilyDeliveries).find(
-      { where: d => d.status.isEqualTo(WeeklyFamilyDeliveryStatus.Pack.id) });
+      { where: d => d.status.IsGreaterOrEqualTo(WeeklyFamilyDeliveryStatus.Pack.id).and(d.status.IsLessOrEqualTo(WeeklyFamilyDeliveryStatus.Ready.id)) });
   }
+
   static route: Route = {
     path: 'weekly-packer-by-family',
     component: WeeklyPackerByFamilyComponent,
@@ -29,12 +30,30 @@ export class WeeklyPackerByFamilyComponent implements OnInit {
   getDeliveryName(d: WeeklyFamilyDeliveries) {
     return this.context.for(WeeklyFamilies).lookup(f => f.id.isEqualTo(d.familyId)).codeName.value;
   }
-
+  currentDelivery: WeeklyFamilyDeliveries;
   async showDelivery(d: WeeklyFamilyDeliveries) {
+    this.currentDelivery = d;
     this.deliveryProducts = await this.context.for(WeeklyFamilyDeliveryProductStats).find(
       {
         where: x => x.delivery.isEqualTo(d.id).and(x.requestQuanity.IsGreaterOrEqualTo(1)),
         orderBy: dp => [dp.productOrder, dp.productName]
       });
+  }
+  showReadyToPickup() {
+    return this.currentDelivery && this.currentDelivery.status.listValue == WeeklyFamilyDeliveryStatus.Pack;
+  }
+  showReturnToPack() {
+    return this.currentDelivery && this.currentDelivery.status.listValue == WeeklyFamilyDeliveryStatus.Ready;
+
+  }
+  readyToPickupDisabled() {
+    let hasQ = this.deliveryProducts.find(x => x.Quantity.value > 0);
+    return !hasQ;
+  }
+  clickReturnToPack() {
+    this.currentDelivery.changeStatus(WeeklyFamilyDeliveryStatus.Pack);
+  }
+  clickReadyToPickup() {
+    this.currentDelivery.changeStatus(WeeklyFamilyDeliveryStatus.Ready);
   }
 }
