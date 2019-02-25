e60942e8e2fd3a030b97178cbb58bcbe8cc3e34c
Noam <noam@fireflymigration.com>
2019-02-25 08:08:24 +0200

    stam build error

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,4 +1,4 @@
-import { IdEntity, Id, StringColumn, NumberColumn, buildSql } from "../model-shared/types";
+import { IdEntity, Id, StringColumn, NumberColumn } from "../model-shared/types";
 import { EntityClass, Context, ServerContext } from "../shared/context";
 import { BoolColumn } from "radweb";
 import { WeeklyFamilyDeliveryProducts, WeeklyFamilyDeliveries, WeeklyFamilyDeliveryStatus, ProductId } from "../weekly-families-deliveries/weekly-families-deliveries.component";
