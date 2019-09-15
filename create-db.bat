15038447e799fa9334716cde58f45b3e7a2d6630
Noam <noam@fireflymigration.com>
2019-09-15 09:23:20 +0300

    some scripts on upgrade etc...

--- create-db.bat
+++ create-db.bat
@@ -2,6 +2,8 @@
 
 rem remember these for future use
 heroku addons:create heroku-postgresql:DBTYPE
+
+heroku addons:create heroku-postgresql:hobby-dev
 heroku pg:wait
 heroku maintenance:on
 heroku pg:copy DATABASE_URL HEROKU_POSTGRESQL_PINK --app sushi
@@ -11,3 +13,5 @@ heroku addons:destroy HEROKU_POSTGRESQL_LAVENDER
 
 copy from one db to another
 heroku pg:copy app1_name::HEROKU_POSTGRESQL_ONYX_URL HEROKU_POSTGRESQL_AQUA_URL --app app2_name
+
+
