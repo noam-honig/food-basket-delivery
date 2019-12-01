5e73d19f5a5cee8745aaec8e5e2b043adbc9629c
Noam <noam@fireflymigration.com>
2019-12-01 11:59:39 +0200

    Added support for redirect

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -24,147 +24,154 @@ serverInit().then(async (dataSource) => {
 
 
     let app = express();
-    if (!process.env.DISABLE_SERVER_EVENTS) {
-        let serverEvents = new ServerEvents(app);
-        if (Sites.multipleSites) {
-            for (const s of Sites.schemas) {
-                serverEvents.registerPath('/' + s + '/api');
-            }
-        }
-        else {
-            serverEvents.registerPath('/api');
-        }
-
-
-        Families.SendMessageToBrowsers = (x, c) => serverEvents.SendMessage(x, c);
-    }
-    app.get('/data-migration', async (req, res) => {
-        await dataMigration(res);
-    });
-
-    let eb = new ExpressBridge(
-        //@ts-ignore
-        app,
-        dataSource, process.env.DISABLE_HTTPS == "true", !Sites.multipleSites);
-    Helpers.helper = new JWTCookieAuthorizationHelper(eb, process.env.TOKEN_SIGN_KEY);
-
-    function getContext(req: express.Request, sendDs?: (ds: PostgresDataProvider) => void) {
-        //@ts-ignore
-        let r = new ExpressRequestBridgeToDataApiRequest(req);
-        let context = new ServerContext();
-        context.setReq(r);
-        let ds = dataSource(context);
-        context.setDataProvider(ds);
-        if (sendDs)
-            sendDs(ds);
-        return context;
-    }
-    if (Sites.multipleSites)
-        for (const schema of Sites.schemas) {
-            let area = eb.addArea('/' + schema + '/api', async req => {
-                if (req.user) {
-                    let context = new ServerContext();
-                    context.setReq(req);
-                    if (!context.isAllowed(Sites.getOrgRole(context)))
-                        req.user = undefined;
+    let redirect = process.env.REDIRECT;
+    if (redirect) {
+        app.use('/*', async (req, res) => {
+            let to = redirect+req.originalUrl;
+            await res.redirect(to);
+        });
+    } else {
+        if (!process.env.DISABLE_SERVER_EVENTS) {
+            let serverEvents = new ServerEvents(app);
+            if (Sites.multipleSites) {
+                for (const s of Sites.schemas) {
+                    serverEvents.registerPath('/' + s + '/api');
                 }
-            });
-            registerActionsOnServer(area, dataSource);
-            registerEntitiesOnServer(area, dataSource);
-            registerImageUrls(app, getContext, '/' + schema);
-        }
-    else {
-        registerImageUrls(app, getContext, '');
-    }
-    async function sendIndex(res: express.Response, req: express.Request) {
-        let context = getContext(req);
-        let org = Sites.getOrganizationFromContext(context);
-        if (!Sites.isValidOrganization(org)) {
-            res.redirect('/' + Sites.guestSchema + '/');
-            return;
-        }
-        const index = 'dist/index.html';
-
-        if (fs.existsSync(index)) {
-            let x = '';
-            x = (await ApplicationSettings.getAsync(context)).organisationName.value;
-            let result = fs.readFileSync(index).toString().replace('!TITLE!', x).replace("/*!SITE!*/", "multiSite=" + Sites.multipleSites);
-            if (Sites.multipleSites)
-            {
-                result = result.replace('"favicon.ico','"/'+org+'/favicon.ico')
-                .replace('"/assets/apple-touch-icon.png"','"/'+org+'/assets/apple-touch-icon.png"');
             }
-            res.send(result);
+            else {
+                serverEvents.registerPath('/api');
+            }
+
+
+            Families.SendMessageToBrowsers = (x, c) => serverEvents.SendMessage(x, c);
         }
+        app.get('/data-migration', async (req, res) => {
+            await dataMigration(res);
+        });
+
+        let eb = new ExpressBridge(
+            //@ts-ignore
+            app,
+            dataSource, process.env.DISABLE_HTTPS == "true", !Sites.multipleSites);
+        Helpers.helper = new JWTCookieAuthorizationHelper(eb, process.env.TOKEN_SIGN_KEY);
+
+        function getContext(req: express.Request, sendDs?: (ds: PostgresDataProvider) => void) {
+            //@ts-ignore
+            let r = new ExpressRequestBridgeToDataApiRequest(req);
+            let context = new ServerContext();
+            context.setReq(r);
+            let ds = dataSource(context);
+            context.setDataProvider(ds);
+            if (sendDs)
+                sendDs(ds);
+            return context;
+        }
+        if (Sites.multipleSites)
+            for (const schema of Sites.schemas) {
+                let area = eb.addArea('/' + schema + '/api', async req => {
+                    if (req.user) {
+                        let context = new ServerContext();
+                        context.setReq(req);
+                        if (!context.isAllowed(Sites.getOrgRole(context)))
+                            req.user = undefined;
+                    }
+                });
+                registerActionsOnServer(area, dataSource);
+                registerEntitiesOnServer(area, dataSource);
+                registerImageUrls(app, getContext, '/' + schema);
+            }
         else {
-            res.send('No Result' + fs.realpathSync(index));
+            registerImageUrls(app, getContext, '');
         }
+        async function sendIndex(res: express.Response, req: express.Request) {
+            let context = getContext(req);
+            let org = Sites.getOrganizationFromContext(context);
+            if (!Sites.isValidOrganization(org)) {
+                res.redirect('/' + Sites.guestSchema + '/');
+                return;
+            }
+            const index = 'dist/index.html';
+
+            if (fs.existsSync(index)) {
+                let x = '';
+                x = (await ApplicationSettings.getAsync(context)).organisationName.value;
+                let result = fs.readFileSync(index).toString().replace('!TITLE!', x).replace("/*!SITE!*/", "multiSite=" + Sites.multipleSites);
+                if (Sites.multipleSites) {
+                    result = result.replace('"favicon.ico', '"/' + org + '/favicon.ico')
+                        .replace('"/assets/apple-touch-icon.png"', '"/' + org + '/assets/apple-touch-icon.png"');
+                }
+                res.send(result);
+            }
+            else {
+                res.send('No Result' + fs.realpathSync(index));
+            }
+        }
+        app.get('/monitor-report', async (req, res) => {
+            let auth = req.header('Authorization');
+            if (auth != process.env.MONITOR_KEY) {
+                res.sendStatus(404);
+                return;
+            }
+            let ds: PostgresDataProvider;
+            let context = getContext(req, x => ds = x);
+            var dsql = ds.getDirectSql();
+            var fromDate = DateColumn.stringToDate(req.query["fromdate"]);
+            var toDate = DateColumn.stringToDate(req.query["todate"]);
+            if (!fromDate)
+                fromDate = new Date();
+            if (!toDate)
+                toDate = new Date();
+            toDate = new Date(toDate.getFullYear(), toDate.getMonth(), toDate.getDate() + 1);
+
+
+            var connections = (await dsql.execute("SELECT count(*) as x FROM pg_stat_activity where datname=current_database()")).rows[0]['x'];
+
+            var familiesInEvent = await context.for(Families).count(f => f.deliverStatus.isInEvent());
+            var totalFamilies = await context.for(Families).count();
+            var sql = new SqlBuilder();
+            var deliveries = await context.for(FamilyDeliveriesStats).count(f => f.deliveryStatusDate.isGreaterOrEqualTo(fromDate).and(f.deliveryStatusDate.isLessThan(toDate)));
+            var fds = new FamilyDeliveriesStats(context);
+            var query = sql.build(
+                'select count (distinct ',
+                fds.courier,
+                ' ) as x from ',
+                fds.__getDbName(),
+                ' where ',
+                fds.deliveryStatusDate.isGreaterOrEqualTo(fromDate).and(fds.deliveryStatusDate.isLessThan(toDate)));
+
+
+            var helpers = (await dsql.execute(query)).rows[0]['x'];
+            await context.for(FamilyDeliveriesStats).count(f => f.deliveryStatusDate.isGreaterOrEqualTo(fromDate).and(f.deliveryStatusDate.isLessThan(toDate)));
+            var onTheWay = await context.for(Families).count(f => f.onTheWayFilter());
+            var settings = await ApplicationSettings.getAsync(context);
+
+            let r: monitorResult = {
+                totalFamilies,
+                familiesInEvent,
+                dbConnections: connections,
+                deliveries,
+                name: settings.organisationName.value,
+                onTheWay,
+                helpers
+
+            };
+            res.json(r);
+        });
+
+        app.get('', (req, res) => {
+
+            sendIndex(res, req);
+        });
+        app.get('/index.html', (req, res) => {
+
+            sendIndex(res, req);
+        });
+        app.use(express.static('dist'));
+
+        app.use('/*', async (req, res) => {
+            await sendIndex(res, req);
+        });
     }
-    app.get('/monitor-report', async (req, res) => {
-        let auth = req.header('Authorization');
-        if (auth != process.env.MONITOR_KEY) {
-            res.sendStatus(404);
-            return;
-        }
-        let ds: PostgresDataProvider;
-        let context = getContext(req, x => ds = x);
-        var dsql = ds.getDirectSql();
-        var fromDate = DateColumn.stringToDate(req.query["fromdate"]);
-        var toDate = DateColumn.stringToDate(req.query["todate"]);
-        if (!fromDate)
-            fromDate = new Date();
-        if (!toDate)
-            toDate = new Date();
-        toDate = new Date(toDate.getFullYear(), toDate.getMonth(), toDate.getDate() + 1);
-
-
-        var connections = (await dsql.execute("SELECT count(*) as x FROM pg_stat_activity where datname=current_database()")).rows[0]['x'];
-
-        var familiesInEvent = await context.for(Families).count(f => f.deliverStatus.isInEvent());
-        var totalFamilies = await context.for(Families).count();
-        var sql = new SqlBuilder();
-        var deliveries = await context.for(FamilyDeliveriesStats).count(f => f.deliveryStatusDate.isGreaterOrEqualTo(fromDate).and(f.deliveryStatusDate.isLessThan(toDate)));
-        var fds = new FamilyDeliveriesStats(context);
-        var query = sql.build(
-            'select count (distinct ',
-            fds.courier,
-            ' ) as x from ',
-            fds.__getDbName(),
-            ' where ',
-            fds.deliveryStatusDate.isGreaterOrEqualTo(fromDate).and(fds.deliveryStatusDate.isLessThan(toDate)));
-
-
-        var helpers = (await dsql.execute(query)).rows[0]['x'];
-        await context.for(FamilyDeliveriesStats).count(f => f.deliveryStatusDate.isGreaterOrEqualTo(fromDate).and(f.deliveryStatusDate.isLessThan(toDate)));
-        var onTheWay = await context.for(Families).count(f => f.onTheWayFilter());
-        var settings = await ApplicationSettings.getAsync(context);
-
-        let r: monitorResult = {
-            totalFamilies,
-            familiesInEvent,
-            dbConnections: connections,
-            deliveries,
-            name: settings.organisationName.value,
-            onTheWay,
-            helpers
-
-        };
-        res.json(r);
-    });
-
-    app.get('', (req, res) => {
-
-        sendIndex(res, req);
-    });
-    app.get('/index.html', (req, res) => {
-
-        sendIndex(res, req);
-    });
-    app.use(express.static('dist'));
-
-    app.use('/*', async (req, res) => {
-        await sendIndex(res, req);
-    });
     let port = process.env.PORT || 3000;
     app.listen(port);
 });
