e9811fbc745b95cae30fd7fbfab2e06c370bd736
Noam <noam@fireflymigration.com>
2020-04-05 15:34:54 +0300

    fixed some performance

--- src/app/server/initSchema.ts
+++ src/app/server/initSchema.ts
@@ -4,7 +4,7 @@ import { Families } from '../families/families';
 import { BasketType } from "../families/BasketType";
 import { ApplicationSettings } from '../manage/ApplicationSettings';
 import { ApplicationImages } from '../manage/ApplicationImages';
-import { ServerContext, SqlDatabase } from '@remult/core';
+import { ServerContext, SqlDatabase, Column } from '@remult/core';
 import '../app.module';
 
 
@@ -19,29 +19,36 @@ import { DistributionCenters } from '../manage/distribution-centers';
 export async function initSchema(pool1: PostgresPool, org: string) {
 
 
-    var dataSource = new SqlDatabase( new PostgresDataProvider(pool1));
+    var dataSource = new SqlDatabase(new PostgresDataProvider(pool1));
     let context = new ServerContext();
     context.setDataProvider(dataSource);
     let sql = new SqlBuilder();
-    
+
     let f = context.for(Families).create();
-    
 
 
-    
+
+
+
+
     //create index for family deliveries if required
     var fd = context.for(FamilyDeliveries).create();
+
     await dataSource.execute(sql.build('create index if not exists fd_1 on ', fd, ' (', [fd.family, fd.deliveryStatusDate, fd.deliverStatus, fd.courier], ')'));
     //create index if required
+    let createIndex = async (name: string, ...columns: Column<any>[]) => {
+        await dataSource.execute(sql.build("create index if not exists ", name, " on ", f, "  (", columns, ")"));
+    }
     await dataSource.execute(sql.build('create index if not exists f_1 on ', f, ' (', [f.courier, f.deliverStatus], ')'));
     await dataSource.execute("create extension if not exists pg_trgm with schema pg_catalog;");
     await dataSource.execute(sql.build('create index if not exists for_like_on_groups on families using gin  (groups gin_trgm_ops)'));
+    await createIndex("for_distribution_status_queries", f.distributionCenter, f.courier, f.deliverStatus);
 
     if ((await context.for(BasketType).count() == 0)) {
         let h = context.for(BasketType).create();
         h.setEmptyIdForNewRow();
         h.name.value = 'רגיל';
-                h.boxes.value = 1;
+        h.boxes.value = 1;
         await h.save();
     }
     if ((await context.for(DistributionCenters).count() == 0)) {
@@ -187,27 +194,27 @@ export async function initSchema(pool1: PostgresPool, org: string) {
 /* remove the blocked basket logic
 
 /*select count(*) count from (select e1.id, e1.name, e1.phone, e1.smsDate, e1.reminderSmsDate, e1.company, e1.totalKm, e1.totalTime, e1.shortUrlKey, e1.eventComment, e1.needEscort, e1.theHelperIAmEscorting, e1.escort,
-							(select count(*) from Families e2 where e2.courier = e1.id and deliverStatus = 0) deliveriesInProgress, 
-							(select count(*) from Families e2 where e2.courier = e1.id and deliverStatus <= 25) allFamilies, 
+							(select count(*) from Families e2 where e2.courier = e1.id and deliverStatus = 0) deliveriesInProgress,
+							(select count(*) from Families e2 where e2.courier = e1.id and deliverStatus <= 25) allFamilies,
 							(select count(*) from Families e2 where e2.courier = e1.id and e2.deliverStatus in (21, 23, 25)) deliveriesWithProblems,
 							(select max(e2.courierAssingTime) from Families e2 where e2.courier = e1.id and  not (e2.deliverStatus in (90, 95))) lastAsignTime,
-							coalesce(  e1.smsDate> (select max(e2.courierAssingTime) from Families e2 where e2.courier = e1.id and  not (e2.deliverStatus in (90, 95))) + interval '-1' day,false) as gotSms 
-							
-							from Helpers e1) 
+							coalesce(  e1.smsDate> (select max(e2.courierAssingTime) from Families e2 where e2.courier = e1.id and  not (e2.deliverStatus in (90, 95))) + interval '-1' day,false) as gotSms
+
+							from Helpers e1)
 							result where deliveriesInProgress >= 1-- and gotSms <> true
 
 drop index x7
 create index x7 on families (courier,deliverstatus)
 
 /*select count(*) count from Families where deliverStatus = 0 and courier = '' and special <> 1
- 
+
  create index x6 on families(special,deliverstatus,courier)
 
 /*select city, families from (select Families.city, count(*) families from Families Families where deliverStatus = 0 and Families.courier = '' group by Families.city) as result Order By families desc
 
 create index x5 on families (deliverstatus,courier,city)
 
-/* select count(*) count 
+/* select count(*) count
  from Families where deliverStatus >= 0 and deliverStatus <= 2 and courier = ''
 --and (select e1.blocked blockedBasket from BasketType e1 where e1.id = Families.basketType limit 1) = false
  and basketType = ''
@@ -216,17 +223,17 @@ create index x5 on families (deliverstatus,courier,city)
 create index x4 on families ( baskettype,courier,deliverstatus)
 
 
-/* select id, name, tz, tz2, familyMembers, birthDate, cast(birthDate + ((extract(year from age(birthDate)) + 1) * interval '1' year) as date) as nextBirthday, basketType, familySource, socialWorker, 
+/* select id, name, tz, tz2, familyMembers, birthDate, cast(birthDate + ((extract(year from age(birthDate)) + 1) * interval '1' year) as date) as nextBirthday, basketType, familySource, socialWorker,
  socialWorkerPhone1, socialWorkerPhone2, groups, special, defaultSelfPickup, iDinExcel, internalComment, address, floor, appartment, entrance, city, addressComment, postalCode, deliveryComments,
  addressApiResult, phone, phone1Description, phone2, phone2Description, deliverStatus, courier, courierComments, deliveryStatusDate, fixedCourier, courierAssignUser, needsWork, needsWorkUser,
  needsWorkDate, courierAssingTime, deliveryStatusUser,
  --(select e1.blocked blockedBasket from BasketType e1 where e1.id = Families.basketType limit 1),
  routeOrder,
- --(select e1.deliverStatus prevStatus from FamilyDeliveries e1 where e1.family = Families.id order by e1.deliveryStatusDate desc limit 1), 
+ --(select e1.deliverStatus prevStatus from FamilyDeliveries e1 where e1.family = Families.id order by e1.deliveryStatusDate desc limit 1),
  --(select e1.deliveryStatusDate prevDate from FamilyDeliveries e1 where e1.family = Families.id order by e1.deliveryStatusDate desc limit 1),
- --(select e1.courierComments prevComment from FamilyDeliveries e1 where e1.family = Families.id order by e1.deliveryStatusDate desc limit 1), 
+ --(select e1.courierComments prevComment from FamilyDeliveries e1 where e1.family = Families.id order by e1.deliveryStatusDate desc limit 1),
  --case when families.courier <> '' then exists (select 1 from FamilyDeliveries where family = families.id and courier = families.courier) else false end courierBeenHereBefore, case when (deliveryStatusDate > current_date -1 or deliverStatus = 0) then true else false end,
- 
+
  addressLongitude, addressLatitude, addressOk, createDate, createUser, lastUpdateDate from Families where deliverStatus < 90 Order By name limit 50 offset 0
 */
 
@@ -234,10 +241,10 @@ create index x4 on families ( baskettype,courier,deliverstatus)
 /*create index xy on families using gin  (groups gin_trgm_ops)
 
 
-select name, familiesCount from 
- (select 
-  groups.name, 
+select name, familiesCount from
+ (select
+  groups.name,
   (select count(*) from Families Families where Families.groups like '%'||groups.name||'%' and deliverStatus = 0 and courier = '' and (select e1.blocked blockedBasket from BasketType e1 where e1.id = Families.basketType limit 1) = false and (select e1.blocked blockedBasket from BasketType e1 where e1.id = Families.basketType limit 1) = false)
   familiesCount from groups groups) result where familiesCount > 0 Order By name limit 1000 offset 0*/
-  
-  /*create index x3 on families ( name,deliverstatus)*/
+
+/*create index x3 on families ( name,deliverstatus)*/
