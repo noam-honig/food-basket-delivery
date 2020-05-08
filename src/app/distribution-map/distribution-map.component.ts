20584e0dc35c4c56bd5cd7ccaccb21b073801837
Noam <noam@fireflymigration.com>
2020-05-08 09:10:51 +0300

    tried dist map with 40000 items

--- src/app/distribution-map/distribution-map.component.ts
+++ src/app/distribution-map/distribution-map.component.ts
@@ -321,7 +321,7 @@ export class DistributionMap implements OnInit, OnDestroy {
         this.bounds.extend(familyOnMap.marker.getPosition());
 
     });
-    if (allInAlll || markers.length > 7000)
+    if (allInAlll || markers.length > 40000)
       var x = new MarkerClusterer(this.map, markers, {
         //imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
         imagePath: 'http://localhost:4200/assets/test',
