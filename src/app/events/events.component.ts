4dc3919c2be33ff5f046d4f15e4ce54130094df9
Noam Honig <noam.honig@gmail.com>
2020-06-08 18:30:24 +0300

    Added show volunteers for event

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Context } from '@remult/core';
-import { Event } from './events';
+import { Event, volunteersInEvent } from './events';
+import { ApplicationSettings } from '../manage/ApplicationSettings';
+import { GridDialogComponent } from '../grid-dialog/grid-dialog.component';
+import { HelperAssignmentComponent } from '../helper-assignment/helper-assignment.component';
+import { Helpers } from '../helpers/helpers';
 
 @Component({
   selector: 'app-events',
@@ -9,7 +13,7 @@ import { Event } from './events';
 })
 export class EventsComponent implements OnInit {
 
-  constructor(private context: Context) { }
+  constructor(private context: Context, private settings: ApplicationSettings) { }
   events = this.context.for(Event).gridSettings({
     allowUpdate: true,
     allowInsert: true,
@@ -19,6 +23,37 @@ export class EventsComponent implements OnInit {
     },
     hideDataArea: true,
     numOfColumnsInGrid: 100,
+    rowButtons: [
+      {
+        name: this.settings.lang.volunteers,
+        click: async e => {
+          this.context.openDialog(GridDialogComponent, x => x.args = {
+            title: e.name.value,
+            settings: this.context.for(volunteersInEvent).gridSettings({
+              get: {
+                limit: 50,
+                where: ve => ve.eventId.isEqualTo(e.id)
+              },
+              rowButtons: [
+                {
+                  name: this.settings.lang.assignDeliveryMenu,
+                  click: async (ev) => {
+                    let h = await this.context.for(Helpers).findId(ev.helper);
+                    await this.context.openDialog(HelperAssignmentComponent, x => x.argsHelper = h);
+                    ev.save();
+                  }
+                }
+              ],
+              columnSettings: ev => [
+                ev.helperName,
+                ev.helperPhone,
+                ev.assignedDeliveries
+              ]
+            })
+          });
+        }
+      }
+    ],
     columnSettings: e => [
       e.name,
       e.eventStatus,
