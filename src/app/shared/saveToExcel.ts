34ec18e078842300d01af6c3cbfdde13c7a1ab72
Noam <noam@fireflymigration.com>
2020-06-21 14:16:45 +0300

    #461

--- src/app/shared/saveToExcel.ts
+++ src/app/shared/saveToExcel.ts
@@ -22,7 +22,7 @@ export async function saveToExcel<E extends Entity<any>, T extends GridSettings<
 
     let wb = XLSX.utils.book_new();
 
-    wb.Workbook = { Views: [{ RTL: use.language.languageCode=='iw' }] };
+    wb.Workbook = { Views: [{ RTL: use.language.languageCode == 'iw' }] };
     let ws = {
 
     } as import('xlsx').WorkSheet;
@@ -41,93 +41,93 @@ export async function saveToExcel<E extends Entity<any>, T extends GridSettings<
     ws["!cols"] = [];
 
 
+    let findOptions = grid.getFilterWithSelectedRows();
+    findOptions.page = 1;
 
-
-    let x = grid.page;
     let rowNum = 2;
     let maxChar = 'A';
 
+    let rows = await context.find(findOptions);
 
-    grid.page = 1;
-    await grid.getRecords();
-    while (grid.items.length > 0) {
-      await foreachSync<Entity<any>>(grid.items
-        , async  f => {
-          let colPrefix = '';
-          let colName = 'A';
-          let colIndex = 0;
+    while (rows.length > 0) {
 
-          let addColumn = (caption: string, v: string, t: import('xlsx').ExcelDataType, hidden?: boolean) => {
+      for (const f of rows) {
+        let colPrefix = '';
+        let colName = 'A';
+        let colIndex = 0;
 
-            if (rowNum == 2) {
-              ws[colPrefix + colName + "1"] = { v: caption };
-              ws["!cols"].push({
-                wch: caption.length,
-                hidden: hidden
-              });
-            }
+        let addColumn = (caption: string, v: string, t: import('xlsx').ExcelDataType, hidden?: boolean) => {
 
-            ws[colPrefix + colName + (rowNum.toString())] = {
-              v: v, t: t
-            };
-            maxChar = colPrefix + colName;
-            {
-              let i = colName.charCodeAt(0);
-              i++;
-              colName = String.fromCharCode(i);
-              if (colName > 'Z') {
-                colName = 'A';
-                if (colPrefix == 'A')
-                  colPrefix = 'B';
-                else if (colPrefix == 'B')
-                  colPrefix = 'C';
-                else
-                  colPrefix = 'A';
-              }
-            }
-            let col = ws["!cols"][colIndex++];
-            if (v) {
-              let len = v.length;
-              if (len > col.wch) {
-                col.wch = len;
-              }
-            }
-          };
-          for (const c of f.columns) {
-            try {
-              if (!excludeColumn(<E>f, c)) {
-                let v = c.displayValue;
-                if (v == undefined)
-                  v = '';
-                let getv: HasAsyncGetTheValue = <any>c as HasAsyncGetTheValue;
-                if (getv && getv.getTheValue) {
-                  v = await getv.getTheValue();
-                }
-
-                if (c instanceof DateTimeColumn) {
-                  addColumn('תאריך ' + c.defs.caption, c.value ? c.getStringForInputDate() : undefined, "d", false);
-                  addColumn('שעת ' + c.defs.caption, c.value ? c.value.getHours().toString() : undefined, "n", false);
-                  addColumn('מלא ' + c.defs.caption, c.displayValue, "s", true);
-                }
-                else
-                  addColumn(c.defs.caption, v.toString(), "s", hideColumn(<E>f, c))
-
-              }
-            } catch (err) {
-
-              console.error(err, c.defs.key, context.toApiPojo(<E>f));
-            }
+          if (rowNum == 2) {
+            ws[colPrefix + colName + "1"] = { v: caption };
+            ws["!cols"].push({
+              wch: caption.length,
+              hidden: hidden
+            });
           }
 
-          if (moreColumns)
-            await moreColumns(<E>f, addColumn);
-          rowNum++;
+          ws[colPrefix + colName + (rowNum.toString())] = {
+            v: v, t: t
+          };
+          maxChar = colPrefix + colName;
+          {
+            let i = colName.charCodeAt(0);
+            i++;
+            colName = String.fromCharCode(i);
+            if (colName > 'Z') {
+              colName = 'A';
+              if (colPrefix == 'A')
+                colPrefix = 'B';
+              else if (colPrefix == 'B')
+                colPrefix = 'C';
+              else
+                colPrefix = 'A';
+            }
+          }
+          let col = ws["!cols"][colIndex++];
+          if (v) {
+            let len = v.length;
+            if (len > col.wch) {
+              col.wch = len;
+            }
+          }
+        };
+        for (const c of f.columns) {
+          try {
+            if (!excludeColumn(<E>f, c)) {
+              let v = c.displayValue;
+              if (v == undefined)
+                v = '';
+              let getv: HasAsyncGetTheValue = <any>c as HasAsyncGetTheValue;
+              if (getv && getv.getTheValue) {
+                v = await getv.getTheValue();
+              }
 
-        });
-      await grid.nextPage();
+              if (c instanceof DateTimeColumn) {
+                addColumn('תאריך ' + c.defs.caption, c.value ? c.getStringForInputDate() : undefined, "d", false);
+                addColumn('שעת ' + c.defs.caption, c.value ? c.value.getHours().toString() : undefined, "n", false);
+                addColumn('מלא ' + c.defs.caption, c.displayValue, "s", true);
+              }
+              else
+                addColumn(c.defs.caption, v.toString(), "s", hideColumn(<E>f, c))
+
+            }
+          } catch (err) {
+
+            console.error(err, c.defs.key, context.toApiPojo(<E>f));
+          }
+        }
+
+        if (moreColumns)
+          await moreColumns(<E>f, addColumn);
+        rowNum++;
+
+      }
+
+      findOptions.page++;
+      rows = await context.find(findOptions);
     }
-    grid.page = x;
-    grid.getRecords();
+
     ws["!ref"] = "A1:" + maxChar + rowNum;
     ws["!autofilter"] = { ref: ws["!ref"] };
 
@@ -143,7 +143,7 @@ export async function jsonToXlsx(busy: BusyService, rows: any[], fileName: strin
 
     let wb = XLSX.utils.book_new();
 
-    wb.Workbook = { Views: [{ RTL: use.language.languageCode=='iw' }] };
+    wb.Workbook = { Views: [{ RTL: use.language.languageCode == 'iw' }] };
     let ws = XLSX.utils.json_to_sheet(rows);
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
     XLSX.writeFile(wb, fileName + '.xlsx');
