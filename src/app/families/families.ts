9d881b7cbc3935e9d36e7a26ea11722335a0a297
Motti Friedman <Motti.Friedman@gmail.com>
2020-08-10 17:31:28 +0300

    fixed docref and adding pixel code

--- src/app/families/families.ts
+++ src/app/families/families.ts
@@ -471,8 +471,6 @@ export class Families extends IdEntity {
   custom2 = new CustomColumn(2);
   custom3 = new CustomColumn(3);
   custom4 = new CustomColumn(4);
-
-  referredBy = new StringColumn();
   
   async reloadGeoCoding() {
 
