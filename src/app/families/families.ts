02efe28c7f1f18ef1a4466ee91de6e32e04201df
Motti Friedman <Motti.Friedman@gmail.com>
2020-08-30 12:22:38 +0300

    .

--- src/app/families/families.ts
+++ src/app/families/families.ts
@@ -252,9 +252,13 @@ export class Families extends IdEntity {
       if (settings.selfPickup)
         fd.deliverStatus.value = DeliveryStatus.SelfPickup;
 
+      '\nsettings.basketType: ' + settings.basketType, 
       console.log("families.addDelivery: \nfamilyID = " + familyId, 
+      '\nsettings.distCenter: ' + settings.distCenter, 
       '\nsettings.basketType: ' + settings.basketType, 
       '\nsettings.quantity: ' + settings.quantity, 
+      '\nsettings.comment: ' + settings.comment, 
+      '\nsettings.courier: ' + settings.courier, 
       '\nsettings.deliverStatus: ' + settings.deliverStatus, 
       '\nsettings.archive: ' + settings.archive);
       await fd.save();
