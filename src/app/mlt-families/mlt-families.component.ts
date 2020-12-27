92ffef964c50be395294a201a9b56b04fbe991cb
Noam <noam@fireflymigration.com>
2020-12-27 10:25:48 +0200

    added the profiler

--- src/app/mlt-families/mlt-families.component.ts
+++ src/app/mlt-families/mlt-families.component.ts
@@ -28,6 +28,7 @@ export class MltFamiliesComponent implements OnInit {
   deliveryList = 'deliveryList';
   deliveryInfo = 'deliveryInfo';
   problemInfo = 'problemInfo';
+  myProfile = 'myProfile';
   display = this.deliveryList;
 
   numberOfDeliveries = 0;
