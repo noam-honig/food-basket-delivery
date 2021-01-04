6754dfde36e2811fe66cd57b51cd002bfe2484a1
Motti Friedman <motti.friedman@gmail.com>
2021-01-04 14:37:49 +0200

    remove FB track in page open

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -84,7 +84,6 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '432475371126727');
-fbq('track', 'Lead');
 </script>
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=432475371126727&ev=PageView&noscript=1"
