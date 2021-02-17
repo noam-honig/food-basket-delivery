81164b0b07d1d3440d1eb5c56dd85b4320565134
RoygEy <roy.gigi@il.ey.com>
2021-02-17 17:09:20 +0200

    #656 changes in volunteers on the way screen

--- src/app/in-route-follow-up/in-route-follow-up.component.ts
+++ src/app/in-route-follow-up/in-route-follow-up.component.ts
@@ -12,6 +12,7 @@ import { DeliveryStatus } from '../families/DeliveryStatus';
 import { saveToExcel } from '../shared/saveToExcel';
 import { ApplicationSettings } from '../manage/ApplicationSettings';
 import { DialogService } from '../select-popup/dialog';
+import { Roles } from '../auth/roles';
 
 @Component({
   selector: 'app-in-route-follow-up',
@@ -20,11 +21,21 @@ import { DialogService } from '../select-popup/dialog';
 })
 export class InRouteFollowUpComponent implements OnInit {
 
-  constructor(private context: Context, private settings: ApplicationSettings, private busy: BusyService,private dialog:DialogService) { }
+  constructor(private context: Context, public settings: ApplicationSettings, private busy: BusyService,private dialog:DialogService) { }
+
+  searchString: string = '';
+  clearSearch() {
+    this.searchString = '';
+    this.helpers.getRecords();
+  }
+
   helpers = this.context.for(InRouteHelpers).gridSettings({
     get: {
       limit: 25,
-      where: x => this.currentOption.where(x)
+      where: h => {
+        let r = h.name.isContains(this.searchString);
+        return r.and(this.currentOption.where(h));
+      }
     },
     knowTotalRows: true,
     showFilter: true,
@@ -34,7 +45,9 @@ export class InRouteFollowUpComponent implements OnInit {
       click: () => saveToExcel(this.settings, this.context.for(InRouteHelpers), this.helpers, "מתנדבים בדרך", this.busy)
     }],
     rowCssClass: x => {
-      if ((x.minAssignDate.value < daysAgo(5)) && (!x.lastCommunicationDate.value || x.lastCommunicationDate.value < daysAgo(5)))
+      if ((!x.seenFirstAssign.value) && (!x.lastCommunicationDate.value || x.lastCommunicationDate.value < daysAgo(3)))
+        return 'communicationProblem';
+      else if ((x.minAssignDate.value < daysAgo(5)) && (!x.lastCommunicationDate.value || x.lastCommunicationDate.value < daysAgo(5)))
         return 'addressProblem';
       else
         return '';
@@ -113,7 +126,12 @@ export class InRouteFollowUpComponent implements OnInit {
         let h = await this.context.for(Helpers).findId(s.id);
         h.displayEditDialog(this.dialog,this.busy);
       }
-    }]
+    },
+    {
+      name: use.language.freezeHelper,
+      visible: () => this.context.isAllowed(Roles.admin)&&this.settings.isSytemForMlt(),
+      click: async h => this.editFreezeDate(h)
+    },]
   });
 
   ngOnInit() {
@@ -129,12 +147,50 @@ export class InRouteFollowUpComponent implements OnInit {
       where: s => s.seenFirstAssign.isEqualTo(false).and(s.minAssignDate.isLessOrEqualTo(daysAgo(2)))
     },
     {
-      text: 'שיוך ראשון לפני יותר מ 7 ימים',
-      where: s => s.minAssignDate.isLessOrEqualTo(daysAgo(7))
+      text: 'שיוך ראשון לפני יותר מ 5 ימים',
+      where: s => s.minAssignDate.isLessOrEqualTo(daysAgo(5))
     }
   ]
   currentOption = this.radioOption[0];
 
+  async doSearch() {
+    if (this.helpers.currentRow && this.helpers.currentRow.wasChanged())
+      return;
+    this.busy.donotWait(async () =>
+      await this.helpers.getRecords());
+  }
+
+  private freezeDateEntry(h: InRouteHelpers) {
+    let r: DataControlInfo<Helpers>[] = [
+      {
+        column: h.frozenTill,
+        width: '150'
+      },
+      {
+        column: h.internalComment,
+        width: '150'
+      },
+    ];
+    return r;
+  }
+
+  async editFreezeDate(h: InRouteHelpers) {
+    this.context.openDialog(InputAreaComponent, x => x.args = {
+      title: use.language.freezeHelper,
+      ok: async () => {
+        let helper = await this.context.for(Helpers).findId(h.id);
+        helper.frozenTill.value = h.frozenTill.value;
+        helper.internalComment.value = h.internalComment.value;
+        await helper.save();
+        this.helpers.getRecords();
+      },
+      cancel: () => {
+      },
+      settings: {
+        columnSettings: () => this.freezeDateEntry(h)
+      }
+    });
+  }
 }
 
 interface filterOptions {
