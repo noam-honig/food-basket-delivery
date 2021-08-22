5e10fb4fc38d394a45ab8ff8cc4b3156e3e3f26e
Noam <noam@fireflymigration.com>
2021-08-22 18:18:45 +0300

    added pixel code

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -104,6 +104,28 @@ s.parentNode.insertBefore(b, s);})();
 </noscript>
                 `);
             }
+            else {
+                result = result.replace(/<!--FACEBOOK_AND_LINKEDIN_PLACEHOLDER-->/g, `
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '4976882262329043');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=4976882262329043&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+`
+);
+            }
             result = result.replace(/GOOGLE_PIXEL_TAG_ID/g, tagid);
 
 
@@ -165,12 +187,12 @@ s.parentNode.insertBefore(b, s);})();
                         url = req.originalUrl;
                     else
                         url = req.path;
-                } 
+                }
                 remult.getSite = () => getSiteFromUrl(url);
                 if (!remult.isAllowed(Sites.getOrgRole(remult)))
                     remult.setUser(undefined);
                 remult.setDataProvider(dataSource(remult));
-                remult.getOrigin = ()=> req.headers['origin'] as string;
+                remult.getOrigin = () => req.headers['origin'] as string;
                 await InitContext(remult, undefined)
             },
 
