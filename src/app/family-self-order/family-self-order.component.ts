48180248297a5b81b78ff077c5422e2c221034b1
Noam Honig <noam.honig@gmail.com>
2022-03-13 11:39:39 +0200

    fixed error in self order

--- src/app/family-self-order/family-self-order.component.ts
+++ src/app/family-self-order/family-self-order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Remult } from 'remult';
+import { Controller, Remult } from 'remult';
 import { ApplicationSettings } from '../manage/ApplicationSettings';
 import { FamilySelfOrderController } from './family-self-order.controller';
 
@@ -9,6 +9,7 @@ import { FamilySelfOrderController } from './family-self-order.controller';
   templateUrl: './family-self-order.component.html',
   styleUrls: ['./family-self-order.component.scss']
 })
+@Controller('family-self-order')
 export class FamilySelfOrderComponent extends FamilySelfOrderController implements OnInit {
 
   constructor(remult: Remult, public settings: ApplicationSettings, private route: ActivatedRoute) {
