8bc18fafc8e6742f429fe5d75064e3e1188fd3b2
Noam Honig <noam.honig@gmail.com>
2022-10-24 15:28:29 +0300

    restricted access

--- src/app/families/FamilyDeliveries.ts
+++ src/app/families/FamilyDeliveries.ts
@@ -923,7 +923,7 @@ function logChanged(col: FieldRef<any>, dateCol: FieldRef<any, Date>, user: IdFi
 }
 
 @Entity<DeliveryChanges>("deliveryChanges", {
-    //allowApiRead: Roles.admin,
+    allowApiRead: Roles.admin,
     defaultOrderBy: {
         changeDate: "desc"
     }
