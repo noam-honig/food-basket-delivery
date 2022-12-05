c5a182b640e7e26d8b42ebaeb6a033205ea23dbb
Noam Honig <noam.honig@gmail.com>
2022-12-05 08:55:11 +0200

    added select statement

--- src/app/sites/sites.ts
+++ src/app/sites/sites.ts
@@ -146,3 +146,14 @@ from test.applicationSettings
 
 
 */
+
+
+/*
+SELECT
+schemaname,
+   relname as "Table",
+   
+   pg_size_pretty(pg_total_relation_size(relid)) As "Size",
+   pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as "External Size"
+   FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC;
+*/
